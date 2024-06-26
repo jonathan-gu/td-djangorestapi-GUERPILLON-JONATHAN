0fcf99174015227ca6921d75a7a6ff5794a38277
Jonathan Guerpillon <jonathanguerpillon@gmail.com>
2024-06-26 09:51:20 +0200

    setup project

---
+++ tddjangorestapi/researchProjectMonitoring/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ResearchprojectmonitoringConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'researchProjectMonitoring'
