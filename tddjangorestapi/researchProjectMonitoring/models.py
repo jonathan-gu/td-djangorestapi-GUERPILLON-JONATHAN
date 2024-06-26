6b2126fd7f361823bf52439671c199538961c9b7
Jonathan Guerpillon <jonathanguerpillon@gmail.com>
2024-06-26 10:06:53 +0200

    create models

--- tddjangorestapi/researchProjectMonitoring/models.py
+++ tddjangorestapi/researchProjectMonitoring/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ResearchProject(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateField(default=timezone.now)
+    end_date_expected = models.DateField(null=True, blank=True)
+    project_leader = models.ForeignKey('Researcher', on_delete=models.SET_NULL, null=True, related_name='projects')
+
+    def __str__(self):
+        return self.title
+
+class Researcher(models.Model):
+    name = models.CharField(max_length=255)
+    specialty = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Publication(models.Model):
+    title = models.CharField(max_length=255)
+    summary = models.TextField()
+    associated_project = models.ForeignKey('ResearchProject', on_delete=models.CASCADE, related_name='publications')
+    publication_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
